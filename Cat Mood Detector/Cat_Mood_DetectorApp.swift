a46635c0290f203247ad7ec0b20f59954fe60b10
sergeysavva <ssavva@theorchard.com>
2024-04-07 14:44:20 +0300

    initial_commit

---
+++ Cat Mood Detector/Cat_Mood_DetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_Mood_DetectorApp.swift
+//  Cat Mood Detector
+//
+//  Created by Sergey Savva on 6.04.24.
+//
+
+import SwiftUI
+
+@main
+struct Cat_Mood_DetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
